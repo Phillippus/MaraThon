70bc1624c80c06c03704d7a115995a3f4722e009
Phillippus <filip.kohutek@gmail.com>
2025-12-08 23:24:22 +0100

    fuckup

--- MaraThon2.py
+++ MaraThon2.py
@@ -370,7 +370,7 @@ def distribute_rooms(doctors_list, wolf_doc_name, previous_assignments=None, man
     # Definícia aktívnych prijímateľov izieb
     active_assignees = [d for d in doctors_list]
     
-    # Ak je dosť ľudí (aspoň 2 okrem primára), primár neberie izby
+    # OPRAVA: Definícia num_workers
     pure_workers = [d for d in doctors_list if d not in ["Kurisova", "Kohutek"]]
     num_workers = len(pure_workers)
     
@@ -403,6 +403,7 @@ def distribute_rooms(doctors_list, wolf_doc_name, previous_assignments=None, man
                 my_prev_rooms.sort(key=lambda x: x[0])
                 
                 for r_obj in my_prev_rooms:
+                    # Prísna kontrola pre RT, voľná pre ostatných
                     if current_beds[doc] + r_obj[1] <= caps.get(doc, 100):
                         assignment[doc].append(r_obj)
                         current_beds[doc] += r_obj[1]
@@ -410,27 +411,38 @@ def distribute_rooms(doctors_list, wolf_doc_name, previous_assignments=None, man
 
     # --- 3. Rozdelenie zvyšných izieb (Fair Share Algorithm) ---
     while available_rooms:
+        # Kandidáti sú len tí, ktorí nemajú plno
         candidates = [d for d in active_assignees if current_beds[d] < caps.get(d, 100)]
         
         if not candidates:
-             candidates = active_assignees
+             candidates = active_assignees # Musíme preťažiť
 
+        # Zoradenie podľa aktuálneho počtu lôžok -> kto má najmenej, dostane
         candidates.sort(key=lambda d: current_beds[d])
         target_doc = candidates[0]
         
         best_room = None
         beds_left = caps.get(target_doc, 100) - current_beds[target_doc]
+        
+        # Hľadáme izbu, čo sa zmestí
         fitting_rooms = [r for r in available_rooms if r[1] <= beds_left]
         
+        # Ak pre RT lekára nie je miesto a má prísny limit, skúsime iného
         if not fitting_rooms and caps.get(target_doc, 100) < 100:
              if len(candidates) > 1:
+                 # Skúsime druhého v poradí (napr. Vidulin)
                  target_doc = candidates[1]
+                 # Prepočet pre nového kandidáta
                  beds_left = caps.get(target_doc, 100) - current_beds[target_doc]
                  fitting_rooms = [r for r in available_rooms if r[1] <= beds_left]
-             else:
+             
+             # Ak stále nič, tak musíme dať hocičo (alebo nechať neobsadené?)
+             # Radšej preťažíme non-RT lekára
+             if not fitting_rooms and caps.get(target_doc, 100) == 100:
                  fitting_rooms = available_rooms
 
         if not fitting_rooms:
+            # Ak sa nedá inak, berieme hocičo dostupné
             fitting_rooms = available_rooms
 
         if assignment[target_doc]:
